bf76158b74e666a27bf97dcd5f7dce1fcddeb147
talha <talhajaved.te@gmail.com>
2023-01-13 16:48:01 +0500

    home and list page

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,13 @@
-import '../styles/globals.css'
+import '../styles/style.css'
+import HomePage from './HomePage'
+import ListingPage from './ListingPage'
 
-export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+
+export default function App() {
+  return (
+  <>
+    <HomePage/>
+    <ListingPage/>
+  </>
+  )
 }
